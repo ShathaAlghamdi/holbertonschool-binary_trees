487cfb75c85d76a092c2dc6a0cee5a8de30ddf2d
Shatha <61396210+ShathaAlghamdi@users.noreply.github.com>
2026-08-18 19:35:20 +0300

    Fixing the bugs

--- binary_trees.h
+++ binary_trees.h
@@ -2,31 +2,56 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
- * @left: Pointer to the left child
- * @right: Pointer to the right child
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
  */
-
- struct binary_tree_s
- {
-     int n;
-     struct binary_tree_s *parent;
-     struct binary_tree_s *left;
-     struct binary_tree_s *right;
-    };
-
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
+/* Task 0 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Task 1 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* Task 2 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* Task 3 */
 void binary_tree_delete(binary_tree_t *tree);
+
+/* Task 4 */
 int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* Task 5 */
+int binary_tree_is_root(const binary_tree_t *node);
+
+/* Task 6 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 7 */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 8 */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Task 9 */
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif
